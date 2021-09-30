9717897f91a3380091db9173aefb4403a24bed00
Karol Niedźwiecki <niedzwiecki7@gmail.com>
2021-09-30 15:27:48 +0200

    Add order price display (OrderForm, OrderSummary, OrderFormConatiner

---
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './OrderSummary.scss';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import { formatPrice } from '../../../utils/formatPrice';
+
+const OrderSummary = ({tripCost, options}) => {
+  const price = calculateTotal(formatPrice(tripCost), options)
+
+    // <h2 className={styles.component}>
+    // Total: <strong>$12,345</strong></h2>
+    return (
+      <h2 className={styles.component}>
+      Total:
+      <strong>
+       ${price}
+      </strong>
+      </h2>
+    );
+};
+
+OrderSummary.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
+
+export default OrderSummary;
