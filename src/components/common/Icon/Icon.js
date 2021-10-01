fe7157c6649f1cba44591cfa932a9f4a32ac410d
Karol Niedźwiecki <niedzwiecki7@gmail.com>
2021-10-01 11:43:25 +0200

    Add Icons - OrderOption

--- src/components/common/Icon/Icon.js
+++ src/components/common/Icon/Icon.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Icon = props => (<i className={`fas fa-${props.name}`}></i>);
+const Icon = props => (<i className={`fas fa-${props.name}`}>{props.children}</i>);
 
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
+  children: PropTypes.array,
 };
 
 export default Icon;
--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import styles from './OrderOption.module.scss';
+import PropTypes from 'prop-types';
+import Icon from '../../common/Icon/Icon';
+import { formatPrice } from '../../../utils/formatPrice';
 
-const OrderOptionIcons = () => (
+const OrderOptionIcons = ({values, required, setOptionValue, currentValue}) => (
 
-  <div>
-    OrderOptionIcons
-  </div>
-);
+  <div
+    className={styles.component}>
+      {!required ? '' : (
+        <div
+          className={styles.icon}
+          value = ''
+          onClick={() => setOptionValue('')}
+        >
+          <Icon name={'time-circle'}>
+            none
+          </Icon>
+        </div>
+      )}
+      {values.map(value => (
+        <div
+          className={currentValue == value.id ? styles.iconActive : styles.icon}
+          key={value.id} value=''
+          onClick={() => setOptionValue(value.id)}
+        >
+          <Icon name={value.icon}>
+          {value.name}
+          ({formatPrice(value.price)})
+          </Icon>
+        </div>
+      ))}
+    </div>
+  );
+
+
+OrderOptionIcons.propTypes = {
+  values: PropTypes.array,
+  required: PropTypes.bool,
+  currentValue: PropTypes.string,
+  setOptionValue: PropTypes.func,
+};
+
 
 export default OrderOptionIcons;
